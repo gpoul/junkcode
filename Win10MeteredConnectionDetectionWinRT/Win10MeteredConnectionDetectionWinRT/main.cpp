7b1f63338c38e8f09eb544ec3b520595ebfa9eff
Gerhard Poul <gerhard.poul@gmail.com>
2018-09-01 20:36:07 +0200

    Add WinRT version of Win10MeteredConnectionDetection

---
+++ Win10MeteredConnectionDetectionWinRT/Win10MeteredConnectionDetectionWinRT/main.cpp
@@ -0,0 +1,27 @@
+﻿// main.cpp : Defines the entry point for the console application.
+//
+
+#pragma comment(lib, "windowsapp")
+#include "pch.h"
+#include <winrt/Windows.Networking.Connectivity.h>
+
+using namespace winrt;
+using namespace winrt::Windows::Networking::Connectivity;
+
+int main()
+{
+    init_apartment();
+
+	// Retrieve connectivity information
+	ConnectionProfile p = NetworkInformation::GetInternetConnectionProfile();
+	ConnectionCost c = p.GetConnectionCost();
+	NetworkCostType nct = c.NetworkCostType();
+
+	// Output cost type
+	if (nct == NetworkCostType::Unrestricted) {
+		printf("Unrestricted\n");
+	}
+	else {
+		printf("Not unrestricted\n");
+	}
+}
